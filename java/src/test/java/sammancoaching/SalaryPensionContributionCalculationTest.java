fddc5793424a3cb8dab2a893c4b601baf3ab6cdf
emily <emily@bacheconsulting.com>
2025-03-20 11:03:42 +0100

    use fake percentages class

--- java/src/test/java/sammancoaching/SalaryPensionContributionCalculationTest.java
+++ java/src/test/java/sammancoaching/SalaryPensionContributionCalculationTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -13,32 +12,13 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class SalaryPensionContributionCalculationTest {
 
     public static final double NORMAL_BASE_SALARY = 60000.0;
-    private SalaryContributionPercentages fakePercentages;
-
-    @BeforeEach
-    public void setUp() {
-        var salaryPercentages = new HashMap<String, Double>();
-        salaryPercentages.put("LONG_TENURE_PERCENTAGE", 3.5);
-        salaryPercentages.put("MEDIUM_TENURE_PERCENTAGE", 2.0);
-        salaryPercentages.put("NO_TENURE_PERCENTAGE", 0.0);
-        salaryPercentages.put("LEADERSHIP_TEAM_PERCENTAGE", 2.5);
-        salaryPercentages.put("MID_SENIORITY_PERCENTAGE", 3.0);
-        salaryPercentages.put("BASE_CONTRIBUTION_RATE", 5.0);
-
-        fakePercentages = new SalaryContributionPercentages(null) {
-            @Override
-            public double lookupValue(String namedConstant) {
-                return salaryPercentages.get(namedConstant);
-            }
-        };
-    }
+    private final SalaryContributionPercentages fakePercentages = FakePercentages.getStandardValues();
 
     @Test
     public void belowZeroSalary_fails() {
         assertThrows(IllegalArgumentException.class, ()-> PensionContributionCalculator.calculatePensionContribution(
                 BigDecimal.valueOf(-1), -1, null, fakePercentages));
     }
-
 
     @Test
     public void juniorEmployeeWithNoTenure_BasicContribution() {
@@ -91,5 +71,6 @@ public class SalaryPensionContributionCalculationTest {
         assertEquals(6600.0,
                 actualContribution.doubleValue(), 0.001);
     }
+
 }
 
